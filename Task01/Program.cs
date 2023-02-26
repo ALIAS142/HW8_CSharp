847a2a801aafc3dae5970c6b92f64c0cd56a406d
Anna <kochurovaav@gmail.com>
2023-02-26 14:17:03 +0200

    Task01 is done

---
+++ Task01/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 1(54): Программа упорядочивает по убыванию элементы каждой строки двумерного массива.
+
+
+int[,] Massiv = GetMatrix (4, 4, 1, 9);
+Print(Massiv);
+Console.WriteLine();
+RowOrder(Massiv);
+Print(Massiv);
+
+
+
+// Рандомный int массив
+int [,] GetMatrix (int rows, int columns, int min, int max)
+{
+int [,] matrix = new int [rows, columns];
+for (int i = 0; i < rows; i++)
+{
+for (int l = 0; l < columns; l++)
+{
+matrix [i, l] = new Random().Next(min, max + 1);
+}
+}
+return matrix;
+}
+
+// Печать массива
+
+void Print(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            System.Console.Write(matrix[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+// Упорядочивание строки
+
+void RowOrder (int[,] matrix)
+
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int l = 0; l < matrix.GetLength(1)-1; l++)
+            {
+                if (matrix[i,l] < matrix [i,l+1])
+                {
+                    int temp = matrix [i,l+1];
+                    matrix [i,l+1] = matrix[i,l];
+                    matrix[i,l] = temp;
+                }
+            }
+            
+        }
+    }
+}
+
+
+
+
